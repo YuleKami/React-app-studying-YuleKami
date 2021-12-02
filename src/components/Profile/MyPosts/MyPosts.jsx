db8757608225bb5a6d2f93022ffb9ddf99766198
YuleKami <Malceew@yandex.ru>
2021-12-02 18:37:35 +0700

    Redux - onClick, refs, virtualDOM, statement

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,27 @@
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
+import React from "react";
 
 const MyPosts = (props) => {
     let postsElements = props.posts.map(p => <Post likecounts={p.likecounts} message={p.message}/>);
+
+    let newPostElement = React.createRef()
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
+
     return (
         <div>
             <div className={s.postsBlock}>
                 my posts
                 <div>
                     <div>
-                        <textarea></textarea>
+                        <textarea ref={newPostElement}/>
                     </div>
                     <div>
-                        <button>Add post</button>
+                        <button onClick={addPost}>Add post</button>
                     </div>
                 </div>
                 <div className={s.posts}>
