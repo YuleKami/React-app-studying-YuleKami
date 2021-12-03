f26a5c858eabcd2ee9ef1e2e2c44175854a7796c
YuleKami <Malceew@yandex.ru>
2021-12-03 12:41:33 +0700

    Callback to props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -21,7 +21,7 @@ const MyPosts = (props) => {
                         <textarea ref={newPostElement}/>
                     </div>
                     <div>
-                        <button onClick={addPost}>Add post</button>
+                        <button onClick={ addPost }>Add post</button>
                     </div>
                 </div>
                 <div className={s.posts}>
