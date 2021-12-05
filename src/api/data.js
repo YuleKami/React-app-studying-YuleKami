88ccaf38fd71510c3bfb5031f4abb6a9e53ff8b7
YuleKami <Malceew@yandex.ru>
2021-12-05 13:06:54 +0700

    OOP ReactJS

--- src/api/data.js
+++ src/api/data.js
@@ -1,64 +1,70 @@
-import {rerenderEntireTree} from "../render";
-
-export let state = {
-    profilePage: {
-        posts: [
-            {id: '1', message: 'Hi, how are you?', likecounts: 10,},
-            {id: '1', message: "Kyk", likecounts: 20},
-            {id: '1', message: "Kek", likecounts: 30},
-            {id: '1', message: "It's my first post!", likecounts: 40},
-        ],
-        newPostText: 'NewPost',
+let store = {
+    state: {
+        profilePage: {
+            posts: [
+                {id: '1', message: 'Hi, how are you?', likecounts: 10,},
+                {id: '1', message: "Kyk", likecounts: 20},
+                {id: '1', message: "Kek", likecounts: 30},
+                {id: '1', message: "It's my first post!", likecounts: 40},
+            ],
+            newPostText: 'NewPost',
+        },
+        dialogsPage: {
+            messages: [
+                {id: '1', message: 'Hi'},
+                {id: '2', message: 'How about play doto?'},
+                {id: '3', message: 'Haha react is good)'},
+                {id: '4', message: 'Oh no, that not good('},
+                {id: '5', message: 'Kek'},
+                {id: '6', message: 'Kyk'},
+            ],
+            newMessageText: 'NewMessage',
+            dialogs: [
+                {id: '1', name: 'YuleKami'},
+                {id: '2', name: 'Portunis'},
+                {id: '3', name: 'Vasily'},
+                {id: '4', name: 'Dthv'},
+                {id: '5', name: 'Vertuass'},
+                {id: '6', name: 'Gremna'},
+            ],
+        },
     },
-    dialogsPage: {
-        messages: [
-            {id: '1', message: 'Hi'},
-            {id: '2', message: 'How about play doto?'},
-            {id: '3', message: 'Haha react is good)'},
-            {id: '4', message: 'Oh no, that not good('},
-            {id: '5', message: 'Kek'},
-            {id: '6', message: 'Kyk'},
-        ],
-        newMessageText: 'NewMessage',
-        dialogs: [
-            {id: '1', name: 'YuleKami'},
-            {id: '2', name: 'Portunis'},
-            {id: '3', name: 'Vasily'},
-            {id: '4', name: 'Dthv'},
-            {id: '5', name: 'Vertuass'},
-            {id: '6', name: 'Gremna'},
-        ],
+    rerenderEntireTree() {
+        console.log('State changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: state.profilePage.newPostText,
+            likecounts: 0,
+        };
+
+        state.profilePage.posts.push(newPost);
+        state.profilePage.newPostText = '';
+        rerenderEntireTree(state);
+    },
+    updateNewPostText(newText) {
+        state.profilePage.newPostText = newText;
+        rerenderEntireTree(state);
+    },
+    addMessage() {
+        let newMessage = {
+            id: 7,
+            message: state.dialogsPage.newMessageText,
+        };
+
+        state.dialogsPage.messages.push(newMessage);
+        state.dialogsPage.newMessageText = '';
+        rerenderEntireTree(state);
+    },
+    subscribe(observer) {
+        rerenderEntireTree = observer;
+    },
+    updateNewMessageText(newText) {
+        state.dialogsPage.newMessageText = newText;
+        rerenderEntireTree(state);
     },
 }
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likecounts: 0,
-    };
 
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export let addMessage = () => {
-    let newMessage = {
-        id: 7,
-        message: state.dialogsPage.newMessageText,
-    };
-
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newMessageText = '';
-    rerenderEntireTree(state);
-}
-
-export let updateNewMessageText = (newText) => {
-    state.dialogsPage.newMessageText = newText;
-    rerenderEntireTree(state);
-}
+export default store;
+window.store = store;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import reportWebVitals from './reportWebVitals';
-import {state} from "./api/data";
-import {rerenderEntireTree} from "./render";
+import {state, subscribe} from "./api/data";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {addMessage, addPost, updateNewMessageText, updateNewPostText} from "./api/data";
+
+export let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state}
+                 addPost={addPost}
+                 updateNewPostText={updateNewPostText}
+                 addMessage={addMessage}
+                 updateNewMessageText={updateNewMessageText}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
